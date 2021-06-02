216db099e9924fe03ac6cf7e9b286dd2707c6a6d
Alex Puz <50154836+alexpuz@users.noreply.github.com>
2021-06-01 21:54:20 -0400

    Moving `CoreDataFeedStore` to a separate file
    :q
    :q!

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -155,14 +155,4 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 //
 //}
 
-final class CoreDataFeedStore: FeedStore {
-	func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-	}
-	
-	func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-	}
-	
-	func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
-	}
-}
+
